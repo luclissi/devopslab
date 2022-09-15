59412bd22146427e8213f3582c69f1ba335cb986
Lucca Ecclissi <lucca.ecclissi@gmail.com>
2022-09-14 23:39:56 -0300

    Finish Commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO"
+    return "Entregua Laboratório DevOps - FIAP 8ASO Grupo 08 <br/>● Lucca Ecclissi - RM342996<br/>● Magno Melo - RM343228<br/>● JT Fernandes - RM343281 "
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
